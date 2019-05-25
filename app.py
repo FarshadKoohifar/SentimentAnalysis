b2d483f37a033d7e11e2b716158bee656b6d6022
Farshad Koohifar <farshad.koohifar@gmail.com>
2019-05-24 22:00:27 -0400

    before Farnaz

--- app.py
+++ app.py
@@ -5,8 +5,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("layout.html")
+    return render_template("index.html")
 
 @app.route('/img/<path:path>')
 def send_img(path):
     return send_from_directory('static/img', path)
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('static/js', path)
+
+
+@app.route('/bootstrap-4.3.1-dist/<path:path>')
+def send_bs(path):
+    return send_from_directory('static/bootstrap-4.3.1-dist', path)
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('static/css', path)
